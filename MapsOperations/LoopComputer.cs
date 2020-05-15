2e089e7502f1078eb990b23178724e0788c6b8a2
AB <pylnik@gmail.com>
2020-05-15 18:59:27 +0200

    Refinements in loops calculator

--- MapsOperations/LoopComputer.cs
+++ MapsOperations/LoopComputer.cs
@@ -14,54 +14,57 @@ namespace MapsOperations
     {
         public float MinLength { get; set; }
         float _minHalfLength;
-        IEnumerable<GeoNode> _nodesCache;
+        Dictionary<long, GeoNode> _nodesCache;
         public DBLayer Dal { get; set; }
+        List<GeoPath> HalfPaths { get; set; }
+        public event Action<Tuple<GeoPath, GeoPath>> NewLoop;
         public void Compute()
         {
             if (Dal == null)
                 throw new NotImplementedException(nameof(Dal));
             _minHalfLength = MinLength / 2;
-            _nodesCache = Dal.GetAllNodes().ToList();
-            var firstNode = _nodesCache.First();
+            _nodesCache = Dal.GetAllNodesDictionary();
+            var firstNode = _nodesCache.Values.First();
+            HalfPaths = new List<GeoPath>();
             //var nodes = Dal.GetAllNodes().ToList();
-            var paths = GetPaths(new List<GeoNode> { firstNode });
-            var loops = GetLoops(paths);
+            BuildTree(new List<GeoNode> { firstNode });
+            var loops = GetLoops(HalfPaths);
             Trace.TraceInformation(loops.Count.ToString());
         }
         private List<Tuple<GeoPath, GeoPath>> GetLoops(List<GeoPath> halfLoops)
         {
             var loops = new List<Tuple<GeoPath, GeoPath>>();
-            IEnumerable<GeoPath> tail = halfLoops.ToList();
-            foreach (var halfLoop in halfLoops)
+            for (int i = 0; i < halfLoops.Count; i++)
             {
+                var halfLoop = halfLoops[i];
                 var lastNode = halfLoop.Nodes.Last();
                 var lastId = lastNode.Id;
-                tail = tail.SkipWhile(p => p == halfLoop);
-                foreach (var half2 in tail)
+                for (int j = i + 1; j < halfLoops.Count; j++)
                 {
+                    var half2 = halfLoops[j];
                     if (half2.Nodes.Last().Id == lastId)
+                    {
                         loops.Add(new Tuple<GeoPath, GeoPath>(halfLoop, half2));
+                        NewLoop?.Invoke(loops.Last());
+                    }
                 }
             }
             return loops;
         }
-        private List<GeoPath> GetPaths(List<GeoNode> nodesToVisit)
+        private void BuildTree(List<GeoNode> nodesToVisit)
         {
-            List<GeoPath> paths = new List<GeoPath>();
             foreach (var node in nodesToVisit)
             {
-                paths.AddRange(TraceNodes(new List<GeoNode> { node }, 0));
+                TraceNodes(new List<GeoNode> { node }, 0);
             }
-            return paths;
         }
 
-        private List<GeoPath> TraceNodes(List<GeoNode> visitedNodes, double curLength)
+        private void TraceNodes(List<GeoNode> visitedNodes, double curLength)
         {
-            var nodes = new List<GeoPath>();
             if (curLength > _minHalfLength)
             {
-                nodes.Add(new GeoPath { Nodes = visitedNodes, Length = (float)curLength });
-                return nodes;
+                HalfPaths.Add(new GeoPath { Nodes = visitedNodes, Length = (float)curLength });
+                return;
             }
             GeoNode nodeFrom = visitedNodes.Last();
             var nodesToVisit = nodeFrom?.NeighbourNodes;
@@ -69,14 +72,13 @@ namespace MapsOperations
             {
                 if (visitedNodes.FirstOrDefault(n => n.Id == nodeTo.NodeId) == null)
                 {
-                    var nodeToFull = _nodesCache.FirstOrDefault(n => n.Id == nodeTo.NodeId);
+                    var nodeToFull = _nodesCache[nodeTo.NodeId];
                     var newVisitedNodes = visitedNodes.ToList();
                     newVisitedNodes.Add(nodeToFull);
-                    var dist = nodeTo.Distance;// GetDistanceBetweenNodes(nodeFrom, nodeToFull);
-                    nodes.AddRange(TraceNodes(newVisitedNodes, curLength + dist));
+                    var dist = nodeTo.Distance;
+                    TraceNodes(newVisitedNodes, curLength + dist);
                 }
             }
-            return nodes;
         }
         private double GetDistanceBetweenNodes(GeoNode node1, GeoNode node2)
         {
